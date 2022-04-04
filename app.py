98239953013fc36fb164c0c86e32e9160131847e
RyanRamos01 <ryanramos@alunos.utfpr.edu.br>
2022-04-04 19:18:43 -0300

    versão final do bot

--- app.py
+++ app.py
@@ -19,17 +19,20 @@ git_integration = GithubIntegration(
     app_key,
 )
 
-def pr_opened_event(repo, payload):
-    pr = repo.get_issue(number=payload['pull_request']['number'])
-    author = pr.user.login
+def delete_branch_after_accepted_rs(repo, branch_name):
+    branch = repo.get_git_ref("heads/%s" % branch_name)
+    branch.delete()
 
-    is_first_pr = repo.get_issues(creator=author).totalCount
+def rs_accepted_event(repo, payload):
+    rs = repo.get_issue(number=payload['pull_request']['number'])
+    contributor = rs.user.login
 
-    if is_first_pr == 1:
-        response = f"Thanks for opening this pull request, @{author}! " \
-                   f"The repository maintainers will look into it ASAP! :speech_balloon:"
-        pr.create_comment(f"{response}")
-        pr.add_to_labels("needs review")
+    response = f"Sua solicitação de PULL REQUEST foi aceita!, @{contributor}! "\
+        f"Agradecido pela contribuição!!!"
+    rs.create_comment(f"{response}")
+
+    branch_name = payload['pull_request']['head']['ref']
+    delete_branch_after_accepted_rs(repo, branch_name)
 
 @app.route("/", methods=['POST'])
 def bot():
@@ -49,8 +52,9 @@ def bot():
     repo = git_connection.get_repo(f"{owner}/{repo_name}")
 
     # Check if the event is a GitHub pull request creation event
-    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
-        pr_opened_event(repo, payload)
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'closed':
+        if payload['pull_request']['merged']:
+            rs_accepted_event(repo, payload)
 
     return "", 204
 
